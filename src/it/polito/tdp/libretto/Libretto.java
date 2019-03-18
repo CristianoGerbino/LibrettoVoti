fe7a85e30baa64e4177f9547822b41b311165ed6
Cristiano <Cristiano@172.22.56.71>
2019-03-18 13:57:03 +0100

    Implementato punto 2

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -19,4 +19,22 @@ public class Libretto {
 	public void add (Voto v) {
 		voti.add(v);
 	}
+	
+	//Per il punto 2 è preferibile utilizzare una funzione che ritorni una lista di voti,
+	//in modo tale da poterla utilizzare anche con un interfaccia grafica
+	
+	public List<Voto> cercaVoti (int voto) {
+		
+		List <Voto> result = new ArrayList <Voto>();
+		
+		for (Voto v :this.voti) {
+			if (v.getPunti()== voto) {
+				result.add(v);
+			}
+		}
+		return result;
+	}
+	
+	
+	
 }
--- src/it/polito/tdp/libretto/TestLibretto.java
+++ src/it/polito/tdp/libretto/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.libretto;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 	//Serve a testare il nostro programma, evitando di utilizzare dati privati di Libretto
@@ -13,7 +14,10 @@ public class TestLibretto {
 		libr.add(new Voto(24, "Fisica II", LocalDate.of(2018, 2, 28)));
 		libr.add(new Voto(28, "Ricerca Operativa", LocalDate.of(2018, 7, 15)));
 		libr.add(new Voto(29, "Programmazione a Oggetti", LocalDate.of(2019, 1, 31)));
-		libr.add(new Voto(21, "Geometria", LocalDate.of(2017, 6, 27)));
+		libr.add(new Voto(25, "Geometria", LocalDate.of(2017, 6, 27)));
+		
+		List<Voto> venticinque = libr.cercaVoti(25);
+		System.out.println(venticinque);
 
 
 	}
